4f87c0b5db1ce3c6d4a244c94cb2027d112453da
willAtaides <sg552.santos@gmail.com>
2026-03-07 15:47:38 -0300

    created general response dto

---
+++ backend/FoodApp/src/main/java/com/will/FoodApp/response/Response.java
@@ -0,0 +1,19 @@
+package com.will.FoodApp.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response<T> {
+
+    private int statusCode;
+    private String message;
+    private T data;
+    private Map<String, Serializable> meta;
+}
